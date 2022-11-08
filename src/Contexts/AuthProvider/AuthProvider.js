793b15c3402a94e6a79aa3518f5ff7c467fa02b2
Abid Hasan <ab1dhasan@outlook.com>
2022-11-08 11:58:42 +0600

    added login functionality

--- src/Contexts/AuthProvider/AuthProvider.js
+++ src/Contexts/AuthProvider/AuthProvider.js
@@ -1,9 +1,23 @@
 import { createContext } from "react";
+import app from "../../firebase/firebase.config";
+import { getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 
 export const AuthContext = createContext();
 
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
 function AuthProvider({ children }) {
-  const authinfo = {};
+  const logIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleSignIn = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const authinfo = { logIn, googleSignIn };
+
   return (
     <AuthContext.Provider value={authinfo}>
       {children}
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,33 @@
+import { useContext } from "react";
+import { AuthContext } from "../../Contexts/AuthProvider/AuthProvider";
 import "./Login.module.css";
 import styles from "./Login.module.css";
 
 function Login() {
+  const { logIn, googleSignIn } = useContext(AuthContext);
+
+  const handleLogIn = async () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    try {
+      const result = await logIn(email, password);
+      console.log(result);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleGoogleLogIn = async () => {
+    try {
+      const result = await googleSignIn();
+      console.log(result);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={e => e.preventDefault()}>
       <h3 className={styles.formTitle}>Login</h3>
       <div className={styles.emailContainer}>
         <label htmlFor="email">Email</label>
@@ -14,7 +38,10 @@ function Login() {
         <input type="password" id="password" name="password" required />
       </div>
       <div>
-        <input className={styles.loginButton} type="submit" value="Login" />
+        <input onClick={handleLogIn} className={styles.loginButton} type="submit" value="Login" />
+      </div>
+      <div>
+        <button onClick={handleGoogleLogIn}>Login with Google</button>
       </div>
     </form>
   );
