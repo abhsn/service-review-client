6940a5203fb8cf8fb8ce065e18a73cbf4eb852d7
Abid Hasan <ab1dhasan@outlook.com>
2022-11-10 06:21:00 +0600

    added add service page

---
+++ src/Pages/AddService/AddService.js
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+import { AuthContext } from "../../Contexts/AuthProvider/AuthProvider";
+import styles from "./AddService.module.css";
+
+function AddService() {
+	const { user } = useContext(AuthContext);
+	const handleAddService = e => {
+		e.preventDefault();
+		const form = e.target;
+		const title = form.title.value;
+		const url = form.url.value;
+		const details = form.details.value;
+		const price = form.price.value;
+		const service = {
+			name: title,
+			details: details,
+			image: url,
+			price: price
+		}
+		fetch('http://localhost:5000/add-service', {
+			method: 'POST',
+			headers: {
+				"content-type": "application/json",
+				"authorization": `Bearer ${localStorage.getItem('token')}`,
+				"uid": user.uid
+			},
+			body: JSON.stringify(service)
+		})
+		form.reset();
+	}
+	return (
+		<div className={styles.addServiceContainer}>
+			<h3>Add Custom Service</h3>
+			<form onSubmit={handleAddService} className={styles.addServiceForm}>
+				<input className={styles.addServiceField} type="text" name="title" id="" placeholder="Service Title" required />
+				<input className={styles.addServiceField} type="url" name="url" id="" placeholder="Service Image" required />
+				<textarea className={styles.textarea} type="text" name="details" id="" placeholder="Service Details" required />
+				<input className={styles.addServiceField} type="number" name="price" id="" placeholder="Service Price" required />
+				<div className={styles.submitButtonContainer}>
+					<input className={styles.submitButton} type="submit" value="Add Service" />
+				</div>
+			</form>
+		</div>
+	);
+}
+
+export default AddService;
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import AddService from "../Pages/AddService/AddService";
 import AllServices from "../Pages/AllServices/AllServices";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Login/Login";
@@ -24,6 +25,10 @@ export const routes = createBrowserRouter([
 			{
 				path: '/services/:id',
 				element: <ServiceDetails />
+			},
+			{
+				path: '/add',
+				element: <PrivateRoute><AddService /></PrivateRoute>
 			},
 			{
 				path: '/reviews/:id',
