123b67be74064a23c14ece34b9cc91c314d13177
Abid Hasan <ab1dhasan@outlook.com>
2022-11-10 06:29:30 +0600

    added spinner to login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,19 +1,22 @@
-import { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../../Contexts/AuthProvider/AuthProvider";
 import "./Login.module.css";
 import styles from "./Login.module.css";
 import { FcGoogle } from "react-icons/fc";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { setTile } from "../../utils/setTitle";
+import Spinner from "../../Components/Spinner/Spinner";
 
 function Login() {
 	setTile('Login');
-	const { logIn, googleSignIn, setUnauthorized } = useContext(AuthContext);
+	const { logIn, googleSignIn } = useContext(AuthContext);
+	const [loading, setLoading] = useState(false);
 	const location = useLocation();
 	const navigate = useNavigate();
 	const from = location.state?.from?.pathname || "/";
 
 	const handleLogIn = () => {
+		setLoading(true);
 		const email = document.getElementById('email').value;
 		const password = document.getElementById('password').value;
 		logIn(email, password)
@@ -34,6 +37,7 @@ function Login() {
 				})
 					.then(res => res.json())
 					.then(data => {
+						setLoading(false);
 						localStorage.setItem('token', data.token);
 					});
 				navigate(from, { replace: true });
@@ -41,6 +45,7 @@ function Login() {
 	};
 
 	const handleGoogleLogIn = async () => {
+		setLoading(true);
 		try {
 			const result = await googleSignIn();
 			if (result) {
@@ -59,7 +64,10 @@ function Login() {
 					},
 					body: JSON.stringify(currentUser)
 				})
-					.then(res => res.json())
+					.then(res => {
+						setLoading(false);
+						res.json();
+					})
 					.then(data => {
 						localStorage.setItem('token', data.token);
 					});
@@ -71,27 +79,36 @@ function Login() {
 	};
 
 	return (
-		<form className={styles.form} onSubmit={e => e.preventDefault()}>
-			<h3 className={styles.formTitle}>Login</h3>
-			<div className={styles.emailContainer}>
-				<label htmlFor="email">Email</label>
-				<input type="email" id="email" name="email" required />
-			</div>
-			<div className={styles.passwordContainer}>
-				<label htmlFor="password">Password</label>
-				<input type="password" id="password" name="password" required />
-			</div>
-			<div className={styles.buttonContainer}>
-				<div>
-					<input onClick={handleLogIn} className={styles.loginButton} type="submit" value="Login" />
-				</div>
-				<small>or</small>
-				<div>
-					<button onClick={handleGoogleLogIn} className={styles.loginButton}><span className={styles.googleButton}><FcGoogle /></span>Login with Google</button>
-				</div>
-			</div>
-			<small>Don't have an account? <Link to="/register">Register here</Link>.</small>
-		</form>
+		<React.Fragment>
+			{
+				!loading ?
+					<form className={styles.form} onSubmit={e => e.preventDefault()}>
+						<h3 className={styles.formTitle}>Login</h3>
+						<div className={styles.emailContainer}>
+							<label htmlFor="email">Email</label>
+							<input type="email" id="email" name="email" required />
+						</div>
+						<div className={styles.passwordContainer}>
+							<label htmlFor="password">Password</label>
+							<input type="password" id="password" name="password" required />
+						</div>
+						<div className={styles.buttonContainer}>
+							<div>
+								<input onClick={handleLogIn} className={styles.loginButton} type="submit" value="Login" />
+							</div>
+							<small>or</small>
+							<div>
+								<button onClick={handleGoogleLogIn} className={styles.loginButton}><span className={styles.googleButton}><FcGoogle /></span>Login with Google</button>
+							</div>
+						</div>
+						<small>Don't have an account? <Link to="/register">Register here</Link>.</small>
+					</form>
+					:
+					<div className={styles.spinnerContainer}>
+						<Spinner />
+					</div>
+			}
+		</React.Fragment>
 	);
 }
 
